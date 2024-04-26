d19f94df5ea451bfa088a3b368d2c802a7e006d8
DoudiNCer <doudi@outlook.lv>
2024-04-27 00:22:27 +0800

    Fix: The entered commodity price is checked and converted to floating-point numbers

--- action/AddGoods.py
+++ action/AddGoods.py
@@ -18,6 +18,15 @@ class AddGoods(QtWidgets.QWidget, addGoods.Ui_Dialog):
         goods_name = self.lineEdit_goods_name.text()
         goods_price = self.lineEdit_goods_price.text()
         try:
+            goods_price = float(goods_price)
+        except ValueError:
+            QMessageBox.warning(self, "输入错误", "货物价格应为整数或小数")
+            return
+        if goods_price < 0:
+            QMessageBox.warning(self, "输入错误", "货物价格应大于等于0")
+            return
+
+        try:
             response = requests.post(config.server_address + "/nekoerp/goods",
                                      json={'token': config.token, 'name': goods_name, "price": goods_price})
             json_response = response.json()
